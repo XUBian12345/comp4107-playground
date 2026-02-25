d138631e78b88f9729f3aa290f5ad0ae33f7636a
XUBian12345 <pacohappy@outlook.com>
2026-02-25 17:26:55 +0800

    Direct commit to main (for tiny safe changes)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,6 +13,7 @@ fun main() {
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
     }
+    println()
 }
 
 // comment here
